bb73f7db7cb9f7644510cb9e37d1cc9d2327c6af
LeMagicienToz <maxouiii4.uteza@gmail.com>
2026-02-17 19:02:55 +0100

    script_toolchain 1

---
+++ script_toolchain.sh
@@ -0,0 +1,127 @@
+#Starter + Check
+su - lfs
+cd  $LFS/sources
+
+#Binutils-2.46.0
+tar -xf binutils-2.46.0.tar.xz
+cd binutils-2.46.0
+mkdir -v build
+cd       build
+../configure --prefix=$LFS/tools \
+             --with-sysroot=$LFS \
+             --target=$LFS_TGT   \
+             --disable-nls       \
+             --enable-gprofng=no \
+             --disable-werror    \
+             --enable-new-dtags  \
+             --enable-default-hash-style=gnu
+make
+make install
+
+cd ..
+cd ..
+rm -rf binutils-2.46.0
+
+#GCC-15.2.0
+tar -xf gcc-15.2.0.tar.xz
+cd gcc-15.2.0
+tar -xf ../mpfr-4.2.2.tar.xz
+mv -v mpfr-4.2.2 mpfr
+tar -xf ../gmp-6.3.0.tar.xz
+mv -v gmp-6.3.0 gmp
+tar -xf ../mpc-1.3.1.tar.gz
+mv -v mpc-1.3.1 mpc
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+ ;;
+esac
+mkdir -v build
+cd       build
+../configure                  \
+    --target=$LFS_TGT         \
+    --prefix=$LFS/tools       \
+    --with-glibc-version=2.43 \
+    --with-sysroot=$LFS       \
+    --with-newlib             \
+    --without-headers         \
+    --enable-default-pie      \
+    --enable-default-ssp      \
+    --disable-nls             \
+    --disable-shared          \
+    --disable-multilib        \
+    --disable-threads         \
+    --disable-libatomic       \
+    --disable-libgomp         \
+    --disable-libquadmath     \
+    --disable-libssp          \
+    --disable-libvtv          \
+    --disable-libstdcxx       \
+    --enable-languages=c,c++
+make
+make install
+
+cd ..
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include/limits.h
+
+cd ..
+rm -rf gcc-15.2.0
+
+#Linux-6.18.10 API Headers
+tar -xf linux-6.18.10.tar.xz
+cd linux-6.18.10
+
+make mrproper
+make headers
+find usr/include -type f ! -name '*.h' -delete
+cp -rv usr/include $LFS/usr
+
+cd ..
+rm -rf linux-6.18.10
+
+#Glibc-2.43
+tar -xf glibc-2.43.tar.xz
+cd glibc-2.43
+
+case $(uname -m) in
+    i?86)   ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3
+    ;;
+    x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64
+            ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
+    ;;
+esac
+
+patch -Np1 -i ../glibc-fhs-1.patch
+
+mkdir -v build
+cd       build
+
+echo "rootsbindir=/usr/sbin" > configparms
+
+../configure                             \
+      --prefix=/usr                      \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --disable-nscd                     \
+      libc_cv_slibdir=/usr/lib           \
+      --enable-kernel=5.4
+
+make 
+make DESTDIR=$LFS install
+
+sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
+
+echo 'int main(){}' | $LFS_TGT-gcc -x c - -v -Wl,--verbose &> dummy.log
+readelf -l a.out | grep ': /lib'
+
+grep -E -o "$LFS/lib.*/S?crt[1in].*succeeded" dummy.log
+
+rm -v a.out dummy.log
+cd ..
+cd ..
+rm -rf glibc-2.43
+
+#Libstdc++ from GCC-15.2.0
+
